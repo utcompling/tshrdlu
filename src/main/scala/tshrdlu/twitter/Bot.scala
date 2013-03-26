fb8f0f4b6721d3d504227852793e9cea212146a5
Jason Baldridge <jasonbaldridge@gmail.com>
2013-03-26 08:43:36 -0500

    Reconfiguring actors to enable more flexible and multiple reply behaviors.

--- src/main/scala/tshrdlu/twitter/Bot.scala
+++ src/main/scala/tshrdlu/twitter/Bot.scala
@@ -23,22 +23,33 @@ import collection.JavaConversions._
 /**
  * The main actor for a Bot, which basically performance the actions that a person
  * might do as an active Twitter user.
+ *
+ * The Bot monitors the user stream and dispatches events to the
+ * appropriate actors that have been registered with it. Currently only
+ * attends to updates that are addressed to the user account.
  */
-class Bot extends Actor with ActorLogging {
+class Bot extends Actor with ActorLogging
+with StatusListenerAdaptor with UserStreamListenerAdaptor {
   import Bot._
 
-  val userStream = context.actorOf(Props[UserStreamDispatcher], name = "UserStream")
+  val stream = new TwitterStreamFactory().getInstance
+  stream.addListener(this)
+  val username = stream.getScreenName
+
   val twitter = new TwitterFactory().getInstance
-  val simpleReplier = context.actorOf(Props[SimpleReplyCreator], name = "SimpleReplier")
+  val replierManager = context.actorOf(Props[ReplierManager], name = "ReplierManager")
+  val streamReplier = context.actorOf(Props[StreamReplier], name = "StreamReplier")
+  val synonymReplier = context.actorOf(Props[SynonymReplier], name = "SynonymReplier")
+
+  override def preStart {
+    replierManager ! RegisterReplier(streamReplier)
+    replierManager ! RegisterReplier(synonymReplier)
+  }
 
   def receive = {
-    case Start => 
-      userStream ! RegisterReplier(simpleReplier)
-      userStream ! MonitorUserStream(true)
-      userStream ! MonitorUserStream(false)
-      userStream ! MonitorUserStream(true)
+    case Start => stream.user
 
-    case Shutdown => userStream ! Shutdown
+    case Shutdown => stream.shutdown
 
     case SearchTwitter(query) => 
       val tweets: Seq[Status] = twitter.search(query).getTweets.toSeq
@@ -48,6 +59,16 @@ class Bot extends Actor with ActorLogging {
       log.info("Posting update: " + update.getStatus)
       twitter.updateStatus(update)
   }
+
+  override def onStatus(status: Status) {
+    log.info("New status: " + status.getText)
+    val replyName = status.getInReplyToScreenName
+    if (replyName == username) {
+      log.info("Replying to: " + status.getText)
+      replierManager ! ReplyToStatus(status)
+    }
+  }
+
 }
 
 /**
@@ -75,36 +96,134 @@ object Bot {
 
 }
 
-/**
- * An actor that monitors the user stream and dispatches events to the
- * appropriate actors that have been registered with it. Currently only
- * attends to updates that are addressed to the user account.
- */
-class UserStreamDispatcher extends Actor 
-with StatusListenerAdaptor with UserStreamListenerAdaptor with ActorLogging {
+
+class ReplierManager extends Actor with ActorLogging {
   import Bot._
 
-  val stream = new TwitterStreamFactory().getInstance
-  val username = stream.getScreenName
-
-  stream.addListener(this)
-
+  import context.dispatcher
+  import akka.pattern.ask
+  import akka.util._
+  import scala.concurrent.duration._
+  import scala.concurrent.Future
+  import scala.util.{Success,Failure}
+  implicit val timeout = Timeout(10 seconds)
   var repliers = Vector.empty[ActorRef]
 
   def receive = {
-    case MonitorUserStream(listen) => 
-      if (listen) stream.user else stream.shutdown
+    case RegisterReplier(replier) => 
+      repliers = repliers :+ replier
 
-    case RegisterReplier(replier) => repliers = repliers :+ replier
+    case ReplyToStatus(status) =>
+
+      val reply: Future[StatusUpdate] = (repliers(0) ? ReplyToStatus(status)).mapTo[StatusUpdate]
+      reply onSuccess {
+        case update => 
+          log.info("Success: " + update)
+          context.parent ! UpdateStatus(update)
+      }
+
+      //println("**** 1 " + repliers.length)
+      //val replyFutures: Seq[Future[StatusUpdate]] = 
+      //  repliers.map(r => (r ? ReplyToStatus(status)).mapTo[StatusUpdate])
+      //println("**** 2 " + replyFutures.length) 
+      //println("**** 2.5 " + replyFutures.mkString("\n")) 
+      //
+      //Future.sequence(replyFutures) onComplete {
+      //  case Success(candidates) => 
+      //    println("Candidates: ")
+      //    candidates.foreach(println)
+      //    val update: StatusUpdate = 
+      //      if (candidates.length > 0) candidates.toSet.head
+      //      else randomFillerStatus(status)
+      //    context.parent ! UpdateStatus(update)
+      //  case Failure(failure) =>
+      //    val filler = randomFillerStatus(status)
+      //    println("**** 3 " + filler)
+      //    context.parent ! UpdateStatus(filler)
+      //}
+
   }
 
-  override def onStatus(status: Status) {
-    log.info("New status: " + status.getText)
-    val replyName = status.getInReplyToScreenName
-    if (replyName == username) {
-      log.info("Replying to: " + status.getText)
-      repliers.foreach(_ ! ReplyToStatus(status))
-    }
+  lazy val fillerStatusMessages = Vector(
+    "La de dah de dah...",
+    "I'm getting bored.",
+    "Say what?",
+    "What can I say?",
+    "That's the way it goes, sometimes.",
+    "Could you rephrase that?",
+    "Oh well.",
+    "Yawn.",
+    "I'm so tired.",
+    "I seriously need an upgrade.",
+    "I'm afraid I can't do that.",
+    "What the heck? This is just ridiculous.",
+    "Don't upset the Wookiee!",
+    "Hmm... let me think about that.",
+    "I don't know what to say to that.",
+    "I wish I could help!",
+    "Make me a sandwich?",
+    "Meh.",
+    "Are you insinuating something?",
+    "If I only had a brain..."
+  )
+
+  lazy val random = new scala.util.Random
+  lazy val numFillers = fillerStatusMessages.length
+
+  def randomFillerStatus(status: Status) = {
+    val text = fillerStatusMessages(random.nextInt(numFillers))
+    val replyName = status.getUser.getScreenName
+    val reply = "@" + replyName + " " + text
+    new StatusUpdate(reply).inReplyToStatusId(status.getId)
+  }
+}
+
+
+/**
+ * An actor that constructs replies to a given status.
+ */
+trait BaseReplier extends Actor with ActorLogging {
+  import Bot._
+  import TwitterRegex._
+  import tshrdlu.util.SimpleTokenizer
+
+  import context.dispatcher
+  import scala.concurrent.Future
+
+  def receive = {
+    case ReplyToStatus(status) => 
+      val replyName = status.getUser.getScreenName
+      val candidatesFuture = getReplies(status, 138-replyName.length)
+      candidatesFuture onSuccess {
+        case candidates => 
+          val reply = "@" + replyName + " " + candidates.toSet.head
+        //context.parent ! UpdateStatus(new StatusUpdate(reply).inReplyToStatusId(status.getId))
+          log.info("Candidate reply: " + reply)
+          val update: StatusUpdate = new StatusUpdate(reply).inReplyToStatusId(status.getId)
+          sender ! update
+      }
+  }
+
+  def getReplies(status: Status, maxLength: Int): Future[Seq[String]]
+
+}
+
+/**
+ * An actor that constructs replies to a given status.
+ */
+class SynonymReplier extends BaseReplier {
+  import Bot._ 
+  import tshrdlu.util.English.synonymize
+  import TwitterRegex._
+
+  import context.dispatcher
+  import scala.concurrent.Future
+
+  def getReplies(status: Status, maxLength: Int = 140): Future[Seq[String]] = {
+    log.info("Trying to reply synonym")
+    val text = stripLeadMention(status.getText).toLowerCase
+    val synTexts = (0 until 10).map(_ => Future(synonymize(text))) 
+    Future.sequence(synTexts).map(_.filter(_.length <= maxLength))
   }
 
 }
@@ -113,7 +232,7 @@ with StatusListenerAdaptor with UserStreamListenerAdaptor with ActorLogging {
 /**
  * An actor that constructs replies to a given status.
  */
-class SimpleReplyCreator extends Actor with ActorLogging {
+class StreamReplier extends BaseReplier {
   import Bot._
   import TwitterRegex._
   import tshrdlu.util.SimpleTokenizer
@@ -122,30 +241,16 @@ class SimpleReplyCreator extends Actor with ActorLogging {
   import akka.pattern.ask
   import akka.util._
   import scala.concurrent.duration._
-  import scala.concurrent.{Future,Await}
+  import scala.concurrent.Future
   implicit val timeout = Timeout(10 seconds)
 
-  def receive = {
-    case ReplyToStatus(status) => 
-      log.info("Replying to: " + status.getText)
-      val replyName = status.getUser.getScreenName
-      
-      val candidatesFuture = getReplies(status, 139-replyName.length)
-      candidatesFuture onSuccess {
-        case candidates => 
-          val reply = "@" + replyName + " " + candidates.toSet.head
-          context.parent ! UpdateStatus(new StatusUpdate(reply).inReplyToStatusId(status.getId))
-      }
-  }
-  
   /**
    * Produce a reply to a status.
    */
   def getReplies(status: Status, maxLength: Int = 140): Future[Seq[String]] = {
-    val text = status.getText.toLowerCase match {
-      case StripLeadMentionRE(withoutMention) => withoutMention
-      case x => x
-    }
+    log.info("Trying to reply stream")
+
+    val text = stripLeadMention(status.getText).toLowerCase
     
     // Get a sequence of futures of status sequences (one seq for each query)
     val statusSeqFutures: Seq[Future[Seq[Status]]] = SimpleTokenizer(text)
@@ -159,7 +264,7 @@ class SimpleReplyCreator extends Actor with ActorLogging {
 
     // Convert this to a Future of a single sequence of candidate replies
     val statusesFuture: Future[Seq[Status]] = 
-      Future.sequence(statusSeqFutures).map(_.flatten).mapTo[Seq[Status]]
+      Future.sequence(statusSeqFutures).map(_.flatten)
 
     // Filter statuses to their text and make sure they are short enough to use.
     statusesFuture.map(_.flatMap(getText).filter(_.length <= maxLength))
@@ -196,7 +301,11 @@ object TwitterRegex {
   lazy val StripLeadMentionRE = """(?:)^@[a-z_0-9]+\s(.*)$""".r
 
   // Pull the RT and mentions from the front of a tweet.
-  lazy val StripMentionsRE = """(?:)(?:RT\s)?(?:(?:@[a-z]+\s))+(.*)$""".r   
+  lazy val StripMentionsRE = """(?:)(?:RT\s)?(?:(?:@[A-Za-z]+\s))+(.*)$""".r   
 
+  def stripLeadMention(text: String) = text match {
+    case StripLeadMentionRE(withoutMention) => withoutMention
+    case x => x
+  }
 
 }
