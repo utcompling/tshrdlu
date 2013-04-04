8585aecceae2585358d17014ad43a633bfce2a91
Jason Baldridge <jasonbaldridge@gmail.com>
2013-04-04 14:47:22 -0500

    Brought back old Project2.scala.

--- src/main/scala/tshrdlu/project/Project2.scala
+++ src/main/scala/tshrdlu/project/Project2.scala
@@ -3,7 +3,6 @@ package tshrdlu.project
 import twitter4j._
 import tshrdlu.twitter._
 import tshrdlu.util.{English,SimpleTokenizer}
-import tshrdlu.util.Polarity
 
 /**
  * Show only tweets that appear to be English.
@@ -29,82 +28,14 @@ trait EnglishStatusListener extends StatusListenerAdaptor {
       println(text)
   }
 
-
-
   /**
    * Test whether a given text is written in English.
    */
-
-val FilterRE = """(a|(?:RT)|[@#].+|.*[",\.\-':;!\?\d\\/<>\(\)\[\]\*$%&=\+#@]+.*)""".r
-
-val lower = "abcdefghijklmnopqrstuvwxyz"
-val upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-// val little2big = lower.zip(upper).toMap
-val big2little = upper.zip(lower).toMap
-
-// def mayCap (letter: Char): Char = {
-//   if (lower.contains(letter)) little2big(letter)
-//   else letter
-// }
-
-def mayLow (letter: Char): Char = {
-  if (upper.contains(letter)) big2little(letter)
-  else letter
-}
-
-def containsWord (word: String): Boolean = {
-
-// val cap1stLetter = mayCap(word.head) + word.tail.map(x=>mayLow(x)).mkString("")
-// val allCaps = word.map(x => mayCap(x)).mkString("")
-val allLows = word.map(x => mayLow(x)).mkString("")
-
-if (English.vocabulary(allLows)) true
-else false
-
-// if (English.vocabulary(word)|English.vocabulary(cap1stLetter)|
-// English.vocabulary(allCaps)|English.vocabulary(allLows)) true
-// else false
-
-}
-
-
-def getRealWords(text: String): IndexedSeq[String] = {
-  SimpleTokenizer(text)
-    .flatMap( x => x match { 
-      case FilterRE(x) => None
-      case _ => Some(x)
-      })
+  val TheRE = """(?i)\bthe\b""".r // Throw me away!
+  def isEnglish(text: String) = {
+    // Remove this and do better.
+    !TheRE.findFirstIn(text).isEmpty
   }
-
-def isEnglish(text: String) = {
-  val words = getRealWords(text)
-  val total = if (words.length == 0) 1
-            else words.length
-  val numEng = (for (word <- words) yield containsWord(word))
-  .filter(x=>x==true).length
-  val prop = (numEng.toDouble)/total
-
-  if (prop >= 0.40) true
-  else false
-
-}
-
-// def isEnglishX (text: String) = {
-
-// val wordResults = SimpleTokenizer(text)
-//     .map(x=>x match { 
-//       case FilterRE(x) => (x, "JUNK")
-//       case _ => (x, containsWord(x))
-//       })
-
-// val numAll = wordResults.filter(x=>x._2 !="JUNK").length
-// val numEng = wordResults.filter(x=>x._2 !="JUNK").filter(x=>containsWord(x._1)==true).length
-// val prop = numEng.toDouble/(numAll+.00001)
-
-// (wordResults, numEng, numAll, prop)
-
-// }
-
 
 }
 
@@ -121,15 +52,9 @@ trait EnglishStatusListenerDebug extends EnglishStatusListener {
    */
   override def onStatus(status: Status) { 
     val text = status.getText
-    val prefix = if (isEnglish(text)) "[ENGLISH]" else "[OTHER]"
+    val prefix = if (isEnglish(text)) "[ENGLISH]" else "[OTHER]  "
     println(prefix + " " + text)
-
   }
-
-  // val testTweet = "Que intenso!"
-  // println(isEnglishX(testTweet))
-  // println("VERDICT :" + isEnglish(testTweet))
-
 }
 
 
@@ -145,19 +70,14 @@ object PolarityStatusStreamer extends BaseStreamer with PolarityStatusListener
  * statistics at default interval (every 100 tweets). Filtered by provided
  * query terms.
  */
-object PolarityTermStreamer extends FilteredStreamer with TermFilter with PolarityStatusListener
+object PolarityTermStreamer 
 
 /**
  * Output polarity labels for every English tweet and output polarity
  * statistics at an interval of every ten tweets. Filtered by provided
  * query locations.
  */
-object PolarityLocationStreamer extends FilteredStreamer with LocationFilter with PolarityStatusListener {
-
-  override val outputInterval = 10
-}
-
-
+object PolarityLocationStreamer 
 
 
 /**
@@ -180,14 +100,13 @@ trait PolarityStatusListener extends EnglishStatusListener {
 
   override def onStatus(status: Status) {
     val text = status.getText
-    if (isEnglish(text)==true) {
+    if (isEnglish(text)) {
       val polarityIndex = getPolarity(text)
       polarityCounts(polarityIndex) += 1
 
       numEnglishTweets += 1
 
       println(polaritySymbol(polarityIndex) + ": " + text)
-
 
       if ((numEnglishTweets % outputInterval) == 0) {
 	println("----------------------------------------")
@@ -207,31 +126,9 @@ trait PolarityStatusListener extends EnglishStatusListener {
    *   1 for negative
    *   2 for neutral
    */
-
-  def mayLowWord(word:String) = {
-    word.map(x=>mayLow(x)).mkString("")
-  }
-
+  val random = new scala.util.Random
   def getPolarity(text: String) = {
-
-
-    val pos = getRealWords(text).filter(word=>
-      Polarity.posWords(mayLowWord(word))).length
-    val neg = getRealWords(text).filter(word=>
-      Polarity.negWords(mayLowWord(word))).length
-    if (pos > neg) 0
-    else if (pos < neg) 1
-    else 2
+    random.nextInt(3)
   }
 
 }
-
-object Polarity extends Polarity
-
-
-
-
-
-  // val TheRE = """(?i)\bthe\b""".r // Throw me away!
-  //   // Remove this and do better.
-  //   !TheRE.findFirstIn(text).isEmpty
