f5ce59b272ffddd323270994e5cd9dbc26759464
emoney <emoney33@gmail.com>
2013-04-05 13:08:14 -0500

    Maybe have first part of P4 done

--- src/main/scala/tshrdlu/twitter/Replier.scala
+++ src/main/scala/tshrdlu/twitter/Replier.scala
@@ -52,6 +52,112 @@ class SynonymReplier extends BaseReplier {
 
 /**
  * An actor that constructs replies to a given status.
+ * For best results, tweet at me something related to one of the 
+ * topics from the "20 Newsgroups" data
+ * e.g. Religion, baseball, atheism, windows, hockey, mideast, pc hardware
+ */
+class TopicModelReplier extends BaseReplier {
+  import Bot._ 
+  import TwitterRegex._
+  import tshrdlu.util.SimpleTokenizer
+
+  import context.dispatcher
+  import akka.pattern.ask
+  import akka.util._
+  import scala.concurrent.duration._
+  import scala.concurrent.Future
+  implicit val timeout = Timeout(10 seconds)
+
+  val modeler = new TopicModeler("minTopicKeys.txt")
+
+  def getReplies(status: Status, maxLength: Int = 140): Future[Seq[String]] = {
+    log.info("Trying to reply via topic models")
+    val text = stripLeadMention(status.getText).toLowerCase
+    val statusTopicList = SimpleTokenizer(text)
+						.filter(_.length > 4)
+						.toSet
+						.take(3)
+						.toList
+						.flatMap(w => modeler.wordTopicsMap.get(w))
+						.flatten
+
+	val topicWords:List[String] = statusTopicList.map(topic => modeler.topicWordsMap.getOrElse(topic,Set(" "))).take(4).flatten
+
+	val statusQueryList :List[String] = topicWords
+				.filter(_.length > 4)
+                .filter(_.length < 11)
+	        	.sortBy(- _.length)
+				.distinct
+
+	//val statusList = statusQueryList.flatMap(w => twitter.search(new Query(w)).getTweets)
+
+    // Get a sequence of futures of status sequences (one seq for each query)
+    val statusSeqFutures: Seq[Future[Seq[Status]]] = statusQueryList
+    	.map(w => (context.parent ? SearchTwitter(new Query(w))).mapTo[Seq[Status]])
+
+    // Convert this to a Future of a single sequence of candidate replies
+    val statusesFuture: Future[Seq[Status]] =
+      	Future.sequence(statusSeqFutures).map(_.flatten)
+
+	statusesFuture.map{x => extractText(x, statusTopicList.toSet)}
+  }
+
+  /**
+   * Go through the list of Statuses, filter out the non-English ones and
+   * any that contain (known) vulgar terms, strip mentions from the front,
+   * filter any that have remaining mentions or links, and then return the
+   * head of the set, if it exists.
+   */
+  def extractText(statusList: Seq[Status], statusTopics: Set[String]) = {
+    val useableTweets = statusList
+      .map(_.getText)
+      .map {
+			case StripMentionsRE(rest) => rest
+			case x => x
+      }
+      .filterNot(_.contains('@'))
+      .filterNot(_.contains('/'))
+      .filter(tshrdlu.util.English.isEnglish)
+      .filter(tshrdlu.util.English.isSafe)
+
+    //Use topic model to select response
+    val topicDistributions = for ( tweet <- useableTweets) yield {
+    				SimpleTokenizer(tweet).filter(_.length > 4)
+				.toSet
+				.take(3)
+				.toList
+				.flatMap(w => modeler.wordTopicsMap.get(w))
+				.flatten}
+    
+    val topicSimilarity = topicDistributions.map(ids => ids.toSet.intersect(statusTopics).size * {if(statusTopics.size -ids.toSet.size ==0 ) 1 else (1/math.abs(statusTopics.size - ids.toSet.size)).toDouble})
+    
+    val topTweet = topicSimilarity.toList.zip(useableTweets).maxBy(_._1)._2
+
+    List(topTweet)
+  }
+
+  /**
+   * Go through the list of Statuses, filter out the non-English ones and
+   * any that contain (known) vulgar terms, strip mentions from the front,
+   * filter any that have remaining mentions or links, and then return the
+   * head of the set, if it exists.
+   */
+  def getText(status: Status): Option[String] = {
+    import tshrdlu.util.English.{isEnglish,isSafe}
+
+    val text = status.getText match {
+      case StripMentionsRE(rest) => rest
+      case x => x
+    }
+    
+    if (!text.contains('@') && !text.contains('/') && isEnglish(text) && isSafe(text))
+      Some(text)
+    else None
+  }
+}
+
+/**
+ * An actor that constructs replies to a given status.
  */
 class StreamReplier extends BaseReplier {
   import Bot._
---
+++ src/main/scala/tshrdlu/twitter/TopicModeler.scala
@@ -0,0 +1,30 @@
+package tshrdlu.twitter
+
+import io.Source
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eric
+ * Date: 3/7/13
+ * Time: 9:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+class TopicModeler(val inputFile: String) {
+  def inputIterator = Source.fromFile(inputFile).getLines.toList
+
+  lazy val tuples = inputIterator.map { line =>
+    val split = line.split(" ")
+    (split(0) -> split(1))
+  }
+
+  lazy val wordTopicsMap = tuples.groupBy(e => e._1).mapValues(e => e.map(x => x._2).toSet)
+
+  lazy val tuplesRev = inputIterator.map { line =>
+    val split = line.split(" ")
+    (split(1) -> split(0))
+  }
+
+  lazy val topicWordsMap = tuplesRev.groupBy(e => e._1).mapValues(e => e.map(x => x._2).toSet)
+}
